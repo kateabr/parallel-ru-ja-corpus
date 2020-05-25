3bad3a555ec0aa12a47d2c4c25996614fcf50f99
kate <kateabr@yandex.ru>
2020-05-25 18:08:48 +0300

    final version of the aligner

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -81,6 +81,7 @@ class MetricsValuesPos:
 class MetricsValues:
     verb: MetricsValuesPos
     noun: MetricsValuesPos
+    lang: str
 
     def greater_1(self, target):
         return self.get_value_1() > target.get_value_1()
@@ -88,23 +89,75 @@ class MetricsValues:
     def greater_2(self, target):
         return self.get_value_2() > target.get_value_2()
 
+    def greater_3(self, target):
+        if not (self.v_score_is_none() and target.v_score_is_none()):
+            v_sc = self.verb.out_cnt / target.verb.out_cnt > self.verb.in_cnt / target.verb.in_cnt
+        elif self.v_score_is_none() and not target.v_score_is_none() and target.verb.in_cnt > 0:
+            v_sc = True
+        else:
+            v_sc = False
+
+        if not (self.n_score_is_none() and target.n_score_is_none()):
+            n_sc = self.noun.out_cnt / target.noun.out_cnt > self.noun.in_cnt / target.noun.in_cnt
+        elif self.n_score_is_none() and not target.n_score_is_none() and target.noun.in_cnt > 0:
+            n_sc = True
+        else:
+            n_sc = False
+
+        return v_sc or n_sc
+
     def v_score_is_none(self):
-        return not (self.verb.total_cnt > 0 and self.verb.addition_size > 0)
+        return (not self.verb.total_cnt > 0, not self.verb.addition_size > 0)
 
     def n_score_is_none(self):
-        return not (self.noun.total_cnt > 0 and self.noun.addition_size > 0)
+        return (not self.noun.total_cnt > 0, not self.noun.addition_size > 0)
 
     def get_value_1(self):
-        if self.n_score_is_none():
-            if self.v_score_is_none():
+        ns_tot_empty, ns_add_empty = self.n_score_is_none()
+        vs_tot_empty, vs_add_empty = self.v_score_is_none()
+        if ns_tot_empty:
+            if ns_tot_empty:
                 return 1000000.0
-        if not self.n_score_is_none():
-            if self.v_score_is_none():
-                return (2 / self.verb.in_cnt) * (self.noun.out_cnt / self.noun.in_cnt)
-        if not self.v_score_is_none():
-            if self.n_score_is_none():
-                return (2 / self.noun.in_cnt) * (self.verb.out_cnt / self.verb.in_cnt)
-        return (self.noun.out_cnt / self.noun.in_cnt) * (self.verb.out_cnt / self.verb.in_cnt)
+
+        if ns_tot_empty:
+            if self.lang == 'ja':
+                if self.noun.out_cnt == 0:
+                    n_out = 7
+                else:
+                    n_out = self.noun.out_cnt * 1.5
+            elif self.lang == 'ru':
+                if self.noun.out_cnt == 0:
+                    n_out = 2
+                else:
+                    n_out = self.noun.out_cnt * 1.5
+        elif ns_add_empty:
+            if self.lang == 'ja':
+                n_out = 50
+            elif self.lang == 'ru':
+                n_out = 4
+        else:
+            n_out = self.noun.out_cnt
+
+        if vs_tot_empty:
+            if self.lang == 'ja':
+                if self.verb.out_cnt == 0:
+                    v_out = 7
+                else:
+                    v_out = self.verb.out_cnt * 1.5
+            elif self.lang == 'ru':
+                if self.verb.out_cnt == 0:
+                    v_out = 2
+                else:
+                    v_out = self.verb.out_cnt * 1.5
+        elif vs_add_empty:
+            if self.lang == 'ja':
+                v_out = 50
+            elif self.lang == 'ru':
+                v_out = 4
+        else:
+            v_out = self.verb.out_cnt
+
+        return (n_out / self.noun.in_cnt) * (v_out / self.verb.in_cnt)
 
     def get_value_2(self):
         if self.n_score_is_none():
@@ -112,47 +165,73 @@ class MetricsValues:
                 return 1000000.0
         if not self.n_score_is_none():
             if self.v_score_is_none():
-                return (2 / self.noun.in_cnt) * (self.noun.total_cnt / self.noun.in_cnt)
+                return (2 / self.verb.in_cnt) * (self.noun.total_cnt / self.noun.in_cnt)
         if not self.v_score_is_none():
             if self.n_score_is_none():
                 return (2 / self.noun.in_cnt) * (self.verb.total_cnt / self.verb.in_cnt)
         return (self.noun.total_cnt / self.noun.in_cnt) * (self.verb.total_cnt / self.verb.in_cnt)
 
 
-def blank_metrics():
-    return MetricsValues(MetricsValuesPos(in_cnt=1.0, out_cnt=1000000.0, total_cnt=1, addition_size=1),
-                         MetricsValuesPos(in_cnt=1.0, out_cnt=1000000.0, total_cnt=1, addition_size=1))
+# def blank_metrics():
+#     return MetricsValues(MetricsValuesPos(in_cnt=1.0, out_cnt=1000000.0, total_cnt=1, addition_size=1),
+#                          MetricsValuesPos(in_cnt=1.0, out_cnt=1000000.0, total_cnt=1, addition_size=1))
 
 
-def get_stats(ja_src, ru_src):
+def get_stats(ja_src, ru_src, rebalance=True, rebalance_several=False):
     def metrics(ja_base, ru_base, ja_bnd, ru_bnd, ja, ru, current_addition):
-        ru_candidates = [item for item in list(set(sum(ru['VERB'][ru_base:ru_bnd], []))) if item != '']
-        in_cnt = len([item for item in ru_candidates if item in sum(ja['VERB'][ja_base:ja_bnd], [])])
-        # print(f'VERB/in: {in_cnt} :: out: {out_cnt} | {[item for item in ru_candidates if item in\
-        # sum(ja["VERB"][ja_base:ja_bnd], [])]}')
+        res = []
         if current_addition == 'ru':
-            out_cnt = len(ru_candidates) - in_cnt
-            addition_size = len([it for it in ru['VERB'][ru_bnd-1:ru_bnd] if it != ''])
+            for ps in ['VERB', 'NOUN']:
+                candidates_old = list(set([item for item in list(set(sum(ru[ps][ru_base:ru_bnd-1], []))) if item != '']))
+                candidates_new = list(set([item for item in list(set(sum(ru[ps][ru_bnd-1:ru_bnd], []))) if item != '' and
+                                           not item in candidates_old]))
+                in_cnt = len([item for item in candidates_old + candidates_new if item in sum(ja[ps][ja_base:ja_bnd], [])])
+                out_cnt = len(list(set([item for item in candidates_old + candidates_new if item != '' and \
+                                        item not in list(set(sum(ja[ps][ja_base:ja_bnd], [])))])))
+                res.append(MetricsValuesPos(out_cnt=out_cnt, in_cnt=in_cnt + 0.1, total_cnt=len(candidates_old + candidates_new),
+                                            addition_size=len(candidates_new)))
+                print(f'RU:{ps} {in_cnt}/{out_cnt} | {[item for item in candidates_old if item in sum(ja[ps][ja_base:ja_bnd], [])]}'
+                      f' + {[item for item in candidates_new if item in sum(ja[ps][ja_base:ja_bnd], [])]}')
         elif current_addition == 'ja':
-            out_cnt = len(list(set(sum(ja['VERB'][ja_base:ja_bnd], [])))) - in_cnt
-            addition_size = len([it for it in ja['VERB'][ja_bnd - 1:ja_bnd] if it != ''])
-        verb_metrics = MetricsValuesPos(out_cnt=out_cnt, in_cnt=in_cnt + 0.1, total_cnt=len(ru_candidates),
-                                        addition_size=addition_size)
+            for ps in pos:
+                candidates_old = list(set([item for item in list(set(sum(ja[ps][ja_base:ja_bnd-1], []))) if item != '']))
+                candidates_new = list(set([item for item in list(set(sum(ja[ps][ja_bnd-1:ja_bnd], []))) if item != '' and
+                                           not item in candidates_old]))
+                in_cnt = len([item for item in sum(ru[ps][ru_base:ru_bnd], []) if item in candidates_old + candidates_new])
+                out_cnt = len(list(set([item for item in candidates_old + candidates_new if item != '' and\
+                                        item not in list(set(sum(ru[ps][ru_base:ru_bnd], [])))])))
+                res.append(MetricsValuesPos(out_cnt=out_cnt, in_cnt=in_cnt + 0.1, total_cnt=len(candidates_old + candidates_new),
+                                            addition_size=len(candidates_new)))
+                print(f'JA:{ps} {in_cnt}/{out_cnt} | {[item for item in candidates_old if item in sum(ru[ps][ru_base:ru_bnd], [])]}'
+                      f' + {[item for item in candidates_new if item in sum(ru[ps][ru_base:ru_bnd], [])]}')
 
-        ru_candidates = [item for item in list(set(sum(ru['NOUN'][ru_base:ru_bnd], []))) if item != '']
-        in_cnt = len([item for item in ru_candidates if item in sum(ja['NOUN'][ja_base:ja_bnd], [])])
-        if current_addition == 'ru':
-            out_cnt = len(ru_candidates) - in_cnt
-            addition_size = len([it for it in ru['NOUN'][ru_bnd-1:ru_bnd] if it != ''])
-        elif current_addition == 'ja':
-            out_cnt = len(list(set(sum(ja['NOUN'][ja_base:ja_bnd], [])))) - in_cnt
-            addition_size = len([it for it in ja['NOUN'][ja_bnd - 1:ja_bnd] if it != ''])
-        # print(f'NOUN/in: {in_cnt} :: out: {out_cnt} | {[item for item in ru_candidates if item in\
-        # sum(ja["NOUN"][ja_base:ja_bnd], [])]}')
-        noun_metrics = MetricsValuesPos(out_cnt=out_cnt, in_cnt=in_cnt + 0.1, total_cnt=len(ru_candidates),
-                                        addition_size=addition_size)
+        return MetricsValues(verb=res[0], noun=res[1], lang=current_addition)
 
-        return MetricsValues(verb=verb_metrics, noun=noun_metrics)
+
+        #     out_cnt = len(list(set(sum(ja['VERB'][ja_base:ja_bnd], [])))) - in_cnt
+        #     addition_size = len([it for it in ja['VERB'][ja_bnd - 1:ja_bnd] if it != '' and
+        #                          not it in sum(ja['VERB'][ja_base:ja_bnd - 1], [])])
+        # verb_metrics = MetricsValuesPos(out_cnt=out_cnt, in_cnt=in_cnt + 0.1, total_cnt=len(ru_candidates),
+        #                                 addition_size=addition_size)
+        # print(f'VERB/in: {in_cnt} :: out: {out_cnt} |'
+        #       f'{[item for item in ru_candidates if item in sum(ja["VERB"][ja_base:ja_bnd], [])]}')
+        #
+        # ru_candidates = [item for item in list(set(sum(ru['NOUN'][ru_base:ru_bnd], []))) if item != '']
+        # in_cnt = len([item for item in ru_candidates if item in sum(ja['NOUN'][ja_base:ja_bnd], [])])
+        # if current_addition == 'ru':
+        #     out_cnt = len(ru_candidates) - in_cnt
+        #     addition_size = len([it for it in ru['NOUN'][ru_bnd - 1:ru_bnd] if it != '' and
+        #                          not it in sum(ru['NOUN'][ru_base:ru_bnd - 1], [])])
+        # elif current_addition == 'ja':
+        #     out_cnt = len(list(set(sum(ja['NOUN'][ja_base:ja_bnd], [])))) - in_cnt
+        #     addition_size = len([it for it in ja['NOUN'][ja_bnd - 1:ja_bnd] if it != '' and
+        #                          not it in sum(ja['NOUN'][ja_base:ja_bnd - 1], [])])
+        # noun_metrics = MetricsValuesPos(out_cnt=out_cnt, in_cnt=in_cnt + 0.1, total_cnt=len(ru_candidates),
+        #                                 addition_size=addition_size)
+        # print(f'NOUN/in: {in_cnt} :: out: {out_cnt} |'
+        #       f'{[item for item in ru_candidates if item in sum(ja["NOUN"][ja_base:ja_bnd], [])]}')
+        #
+        # return MetricsValues(verb=verb_metrics, noun=noun_metrics)
 
     sent_diff_mean = 2.6962334103151706
     pos = ['VERB', 'NOUN']  # , 'ADJECTIVE']
@@ -168,7 +247,7 @@ def get_stats(ja_src, ru_src):
             ja[ps] = [line.strip().split(',') for line in file.readlines()]
 
     #         ja ru  ja_sc    ru_sc
-    score = [[-1, -1, [blank_metrics(), blank_metrics()]]]
+    score = [[-1, -1, []]]
 
     while True:
         score.append([score[-1][0] + 1, score[-1][1] + 1, []])
@@ -176,7 +255,7 @@ def get_stats(ja_src, ru_src):
         ru_baseline = score[-1][1]
         score[-1][2] = [metrics(ja_baseline, ru_baseline, score[-1][0] + 1, score[-1][1] + 1, ja, ru, 'ja'),
                         metrics(ja_baseline, ru_baseline, score[-1][0] + 1, score[-1][1] + 1, ja, ru, 'ru')]
-        print(f'{score[-1][2][0].get_value_1()} | {score[-1][2][1].get_value_1()}')
+        print(f'RU:{score[-1][2][1].get_value_1()} | JA:{score[-1][2][0].get_value_1()}')
         # print(' ',' '.join(ru_src[ru_baseline:score[-1][1] + 1]), '\n',
         #       ' '.join(ja_src[ja_baseline:score[-1][0] + 1]))
 
@@ -191,8 +270,10 @@ def get_stats(ja_src, ru_src):
             print(f'{score[-1][2][1].get_value_1()} vs {new_metrics.get_value_1()}')
             # print(' ',' '.join(ru_src[ru_baseline:score[-1][1] + 2]), '\n',
             #       ' '.join(ja_src[ja_baseline:score[-1][0] + 1]))
+            #cmp = [score[-1][2][1].greater_1(new_metrics), score[-1][2][1].greater_2(new_metrics), score[-1][2][1].greater_3(new_metrics)]
             if score[-1][2][1].greater_1(new_metrics):
                 score[-1][2][1] = new_metrics
+                score[-1][2][0] = metrics(ja_baseline, ru_baseline, score[-1][0] + 1, score[-1][1] + 2, ja, ru, 'ja')
                 score[-1][1] += 1
                 ja_break_first = False
                 ja_break_last = False
@@ -207,8 +288,10 @@ def get_stats(ja_src, ru_src):
             print(f'{score[-1][2][0].get_value_1()} vs {new_metrics.get_value_1()}')
             # print(' ',' '.join(ru_src[ru_baseline:score[-1][1] + 1]), '\n',
             #       ' '.join(ja_src[ja_baseline:score[-1][0] + 2]))
+            # cmp = [score[-1][2][0].greater_1(new_metrics), score[-1][2][0].greater_2(new_metrics), score[-1][2][0].greater_3(new_metrics)]
             if score[-1][2][0].greater_1(new_metrics):
                 score[-1][2][0] = new_metrics
+                score[-1][2][1] = metrics(ja_baseline, ru_baseline, score[-1][0] + 2, score[-1][1] + 1, ja, ru, 'ru')
                 score[-1][0] += 1
                 ru_break_first = False
                 ru_break_last = False
@@ -223,20 +306,55 @@ def get_stats(ja_src, ru_src):
         if score[-1][0] == len(ja_src) or score[-1][1] == len(ru_src):
             break
 
-    score[-1][0] = len(ja_src)
-    score[-1][1] = len(ru_src)
+    if ' '.join(ru_src[ru_baseline:score[-1][1] + 1]) == ' '.join(ja_src[ja_baseline:score[-1][0] + 1]) == '':
+        score = score[:-1]
+    score[-1][0] = len(ja_src)-1
+    score[-1][1] = len(ru_src)-1
+    score[0][2] = score[1][2]
     res = []
     ja_baseline = 0
     ru_baseline = 0
-    score = score[1:]
-    for id, _ in enumerate(score[:-1]):
-        ja_id = score[id + 1][0] - score[id][0]
-        ru_id = score[id + 1][1] - score[id][1]
-        res.append((list(range(ja_baseline, score[id + 1][0])), list(range(ru_baseline, score[id + 1][1]))))
-        ja_baseline += ja_id
-        ru_baseline += ru_id
 
-    return res
+    for id, _ in enumerate(score[:-1]):
+        res.append([list(range(score[id][0] + 1, score[id + 1][0]+1)),
+                    list(range(score[id][1] + 1, score[id + 1][1]+1)), score[id][2]])
+        # ja_baseline = score[id][0]
+        # ru_baseline = score[id][1]
+
+    if rebalance:
+        for index in range(1, len(res)):
+            if len(res[index][0]) > 1 and res[index][1]:
+                m_without_first_element = metrics(res[index][0][1], res[index][1][0],
+                                                  res[index][0][-1] + 1, res[index][1][-1] + 1,
+                                                  ja, ru, 'ja')
+                while res[index][2][0].greater_1(m_without_first_element):
+                    res[index - 1][0].append(res[index][0][0])
+                    res[index][0] = res[index][0][1:]
+                    res[index][2][0] = m_without_first_element
+                    res[index - 1][2][0] = metrics(res[index - 1][0][0], res[index - 1][1][0],
+                                                   res[index - 1][0][-1] + 1, res[index - 1][1][-1] + 1,
+                                                   ja, ru, 'ja')
+                    if len(res[index][0]) > 1 and rebalance_several:
+                        m_without_first_element = metrics(res[index][0][1], res[index][1][0],
+                                                          res[index][0][-1] + 1, res[index][1][-1] + 1,
+                                                          ja, ru, 'ja')
+            if len(res[index][1]) > 1 and res[index][0]:
+                m_without_first_element = metrics(res[index][0][0], res[index][1][1],
+                                                  res[index][0][-1] + 1, res[index][1][-1] + 1,
+                                                  ja, ru, 'ja')
+                while res[index][2][1].greater_1(m_without_first_element):
+                    res[index - 1][1].append(res[index][1][0])
+                    res[index][1] = res[index][1][1:]
+                    res[index][2][1] = m_without_first_element
+                    res[index - 1][2][1] = metrics(res[index - 1][0][0], res[index][1][0],
+                                                   res[index - 1][0][-1] + 1, res[index - 1][1][-1] + 1,
+                                                   ja, ru, 'ru')
+                    if len(res[index][1]) > 1 and rebalance_several:
+                        m_without_first_element = metrics(res[index][0][0], res[index][1][1],
+                                                          res[index][0][-1] + 1, res[index][1][-1] + 1,
+                                                          ja, ru, 'ru')
+
+    return [it[0:2] for it in res]
 
     #     new_iteration = True
     #     ja_iteration = -1
@@ -344,12 +462,12 @@ if __name__ == '__main__':
 
     # ann_ru([1, 3, 7, 13, 14, 22, 41, 43, 45, 46, 47], mystem, maru_analyzer)
 
-    for i in [1, 3, 7, 13, 14, 22, 41, 43, 45, 46, 47][1:2]:  # [4:]:
+    for i in [1, 3, 7, 13, 14, 22, 41, 43, 45, 46, 47][9:10]:  # [4:]:
         with open(f"../texts/raw/with_ann/{i}_ru.json", 'r') as file:
             ru_src = [it[0] for it in jsonpickle.decode(file.read())]
         with open(f"../texts/raw/with_ann/{i}_ja.json", 'r') as file:
             ja_src = [it[0] for it in jsonpickle.decode(file.read())]
-        ids = get_stats(ja_src, ru_src)
+        ids = get_stats(ja_src, ru_src, rebalance=True, rebalance_several=True)
         for id_pair in ids:
             print(' '.join([ja_src[ja_id] for ja_id in id_pair[0]]), '\n',
                   ' '.join([ru_src[ru_id] for ru_id in id_pair[1]]))
